82506d1673f6ba717bf154196ab71c13245e72e5
mrchughes <76167635+mrchughes@users.noreply.github.com>
2025-06-30 20:47:38 +0100

    feat: migrate Terraform state to S3 backend

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
   region = var.aws_region
 }
 
+
+
 terraform {
   required_version = ">= 1.3.0"
+
+  backend "s3" {
+    bucket         = "mrchughes-terraform-state"
+    key            = "fep-app/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "mrchughes-terraform-lock"
+    encrypt        = true
+  }
 }
