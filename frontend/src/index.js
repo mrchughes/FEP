05d6e50078f68dfb6f182d2c7c70606e25fcf372
mrchughes <76167635+mrchughes@users.noreply.github.com>
2025-06-30 16:43:27 +0100

    Remove govuk.css placeholder file to fix build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./auth/AuthContext";
-import "./styles/govuk.css";
+
 import "./styles/main.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
