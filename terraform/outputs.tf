03fc3b0ae1d865f811acdf740c5c1383aa36208f
mrchughes <76167635+mrchughes@users.noreply.github.com>
2025-06-30 19:14:26 +0100

    fix: remove backend vars, keep S3 bucket for app outputs, use local state

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,9 +6,6 @@ output "frontend_repo_url" {
   value = aws_ecr_repository.frontend.repository_url
 }
 
-output "s3_bucket_name" {
-  value = aws_s3_bucket.fep_output.bucket
-}
 
 output "dynamodb_table_name" {
   value = aws_dynamodb_table.fep_model.name
