df122e6daf41a74a900e7958a59995021bcb4dd2
mrchughes <76167635+mrchughes@users.noreply.github.com>
2025-06-30 18:55:37 +0100

    fix: switch to local backend for Terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,12 +4,4 @@ provider "aws" {
 
 terraform {
   required_version = ">= 1.3.0"
-
-  backend "s3" {
-    bucket         = var.bucket_name
-    key            = "fep-app/terraform.tfstate"
-    region         = var.aws_region
-    dynamodb_table = var.lock_table_name
-    encrypt        = true
-  }
 }
