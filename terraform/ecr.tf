8464dc20f47429e9b70b860225e5c630dfbf8329
mrchughes <76167635+mrchughes@users.noreply.github.com>
2025-06-30 21:01:57 +0100

    chore: finalize Terraform imports and deploy via GitHub Actions

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,7 +1,13 @@
+
+
 resource "aws_ecr_repository" "backend" {
-  name = var.backend_repo_name
+  name                 = var.backend_repo_name
+  image_tag_mutability = "MUTABLE"
+  force_delete         = false  # Add this line
 }
 
 resource "aws_ecr_repository" "frontend" {
-  name = var.frontend_repo_name
+  name                 = var.frontend_repo_name
+  image_tag_mutability = "MUTABLE"
+  force_delete         = false  # Add this line
 }
